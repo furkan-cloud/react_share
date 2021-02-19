2fb4634966225d0f6ada0ae056fd7c7b6e65e08a
furkan-cloud <firstinfirstout2020@gmail.com>
2021-02-19 15:09:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Signup />
+      {/* 
+  
+  //Route
+  //Signin
+  //Signup
+  //forgotpassword
+  //....
+  */}
+    </div>
+  );
+}
+
+export default App;
