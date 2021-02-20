7d5dc38ce19152c7780ff230b6df062636b7a55a
furkan-cloud <firstinfirstout2020@gmail.com>
2021-02-21 01:27:20 +0300

    contect provider added to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import Signup from "./pages/Signup";
-import Navbar from "./components/Navbar";
+import AppRouter from "./router/Router";
+import AuthContextProvider from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Signup />
-      {/* 
-  
-  //Route
-  //Signin
-  //Signup
-  //forgotpassword
-  //....
-  */}
-    </div>
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   );
 }
 
